453ef372aa9d97cefb2097fa29639379225f0ea0
Harsha <harshaswift89@gmail.com>
2024-07-30 09:32:07 +0530

    edit

--- src/components/Button1.jsx
+++ src/components/Button1.jsx
@@ -77,7 +77,7 @@ const Button = ({ styles }) => {
             <div className="relative w-full" style={{ paddingBottom: '56.25%' }}>
               <iframe
                 className="absolute top-0 left-0 w-full h-full"
-                src="https://www.youtube.com/embed/3HyMuNpJBtY"   
+                src="https://www.youtube.com/embed/jpoNgq5yutE "   
                 title="YouTube video player"
                 frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
